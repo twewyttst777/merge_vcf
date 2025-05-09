b26393e3f288301d603560f6f038466c1fd743ba
twewyttst777 <timothyokitsu@gmail.com>
2025-05-09 10:54:42 -0700

    Updated some info

--- README.md
+++ README.md
@@ -1,9 +1,17 @@
+# About
+
+Script to output the union of two vcf files.
+
 main.py can be run with command line arguments to combine two vcf files.
 
 > python3 main.py -i FILE_1 -m FILE_2 -o OUTFILE
 
-Will scan the contents of FILE_1 and output the entries in FILE_2 that share the same chromosomes and starting position.
+This will scan the contents of FILE_1 and output the entries in FILE_2 that share the same chromosomes and starting position.
 
-merge_vcf.sh is a script for batch runs; pass a directory as input and it will run every file in it.
+merge_vcf.sh is a script for batch runs; pass a directory as input and it will run every file in it. Will require modification to run as desired.
 
-Will require modification to run as desired.
+## Installation
+
+> pip3 install -r requirements.txt
+
+Will install required files.
--- merge_vcf.sh
+++ merge_vcf.sh
@@ -4,21 +4,16 @@
 mkdir -p matching
 mkdir -p unmatching
 
-for i in {01..30}
+for FILE in [your_path]/*.HC_All.vcf;
 do
-	for FILE in /media/go/wgsAnalysis/KgpOut_Echo/KUNUSCCLH_00${i}/analysis/*.HC_All.vcf;
-	do
-		PREFIX=${FILE%%.*}
-		NOPATH="${PREFIX##*/}"
-		echo $NOPATH
-		FILE_MUTECT="${PREFIX}_MuTect_All_Filtered.vcf"
-		FILE_SNVS="${PREFIX}.strelka.passed.somatic.snvs.vcf"
-		FILE_INDELS="${PREFIX}.strelka.passed.somatic.indels.vcf"
-		python3 main.py -i $FILE_SNVS -m $FILE_MUTECT -o "${NOPATH}.MuTect_Strelka-snvs.vcf" 
-		python3 main.py -i $FILE_INDELS -m $FILE_MUTECT -o "${NOPATH}.MuTect_Strelka-indels.vcf" 
-	done
-	wait
-			
+	PREFIX=${FILE%%.*}
+	NOPATH="${PREFIX##*/}"
+	echo $NOPATH
+	FILE_MUTECT="${PREFIX}_MuTect_All_Filtered.vcf"
+	FILE_SNVS="${PREFIX}.strelka.passed.somatic.snvs.vcf"
+	FILE_INDELS="${PREFIX}.strelka.passed.somatic.indels.vcf"
+	python3 main.py -i $FILE_SNVS -m $FILE_MUTECT -o "${NOPATH}.MuTect_Strelka-snvs.vcf" 
+	python3 main.py -i $FILE_INDELS -m $FILE_MUTECT -o "${NOPATH}.MuTect_Strelka-indels.vcf" 
 done
 
 
